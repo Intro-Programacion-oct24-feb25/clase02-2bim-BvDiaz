e63c8af3e7e277b7b4f3726aa1435c0dae7a0b35
Bryan Diaz <vicentepomas.2007@gmail.com>
2024-12-11 12:13:01 -0500

    CAMBIOS

--- Ejemplos/src/paquete1/Ejemplo02.java
+++ Ejemplos/src/paquete1/Ejemplo02.java
@@ -27,11 +27,12 @@ public class Ejemplo02 {
         
         // arreglo de tipo double
         double[] arreglo2 = new double[5];
-        System.out.println(arreglo2[0]);
+        System.out.println(arreglo2[0]);   // 0.0 se presenta por ser un arreglo 
+                                           // tipo double
         
         // arreglo de tipo String
         String[] arreglo4 = new String[5];
-        System.out.println(arreglo4[0]);
+        System.out.println(arreglo4[0]);   // null se presenta por la cadena vacia
         
         // arreglo de tipo boleano
         boolean[] arreglo5 = new boolean[5];
